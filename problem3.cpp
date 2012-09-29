5e582a095bf67d83d367fb773b424b257d16cd83
the-lay <ilja.gubin@gmail.com>
2012-09-29 19:46:19 +0300

    Added Problem 3

---
+++ problem3.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+void primeFactor(unsigned long long a)
+{
+    for(unsigned long long i=2; i <= sqrt(a); i++)
+    {
+        if(a % i == 0)
+        {
+        primeFactor(a/i);
+        return;
+        }
+    }
+    cout << a;
+    return;
+}
+
+int main()
+{
+    primeFactor(600851475143LL);
+    return 0;
+}
+
--- problem3_bruteForce.cpp
+++ problem3_bruteForce.cpp
@@ -3,29 +3,21 @@
 
 bool isPrime(int num)
 {
-   for(int i = 2; i < num; i++)
-   {
-      if (num % i == 0 && i != num)
-         return false;
-   }
-   return true;
-}
-
-int returnPrime(int p)
-{
-   std::cout << p << std::endl;
-   return p;
+    for(int i=2; i<num; i++)
+    {
+        if (num%i==0 && i!=num)
+            return false;
+    }
+    return true;
 }
 
 int main()
 {
-   long long int x = 600851475143LL;
+    long long int x = 600851475143LL;
 
-   for(long long int i = 1; i < x; i++)
-   {
-      if(x % i == 0 && isPrime(i) == true)
-      {
-         returnPrime(i);
-      }
-   }
+    for(long long int i=1; i<sqrt(x); i++) //for true brute force make the limit to the x :)
+    {
+        if(x%i == 0 && isPrime(i) == true)
+            std::cout << i << std::endl;
+    }
 }
