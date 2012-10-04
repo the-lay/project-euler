cd1d6261ad351099e2f60b70888989c99d05609c
the-lay <ilja.gubin@gmail.com>
2012-10-04 11:49:52 +0300

    Added 15

---
+++ problem15_cleverMath.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <iomanip> //setprecision()
+using namespace std;
+
+//used combinatoris for this
+//it's the binomial coefficient with 40 on top and 20 on the bottom
+//we have 40 steps to reach the finish(20 for x, 20 for y)
+//so the real problem is:
+//how many different ways can you choose 20 elements out of a set of 40 elements.
+
+long double factorial (int n)
+{
+    long double res=1;
+    for (int i=n; i>0; i--) res*=i;
+    return res;
+}
+
+int main()
+{
+    cout << setprecision (300) << factorial(40)/(factorial(20)*factorial(20));
+}
